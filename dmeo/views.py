deb7b111f09a569edd2c114ffcbd9345c2459f1b
NeelMungra05 <nmungra@deloitte.com>
2023-09-01 16:14:31 +0530

    Cors first

---
+++ dmeo/views.py
@@ -0,0 +1,6 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class Hello(APIView):
+    def get(self,request):
+        return Response({"Hello" : "World"})
